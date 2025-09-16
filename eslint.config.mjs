48ba6273d82b6520eeb55d40095f28c9001d98c1
Oliver Rose <hello@olivermrose.com>
2025-09-16 13:14:40 -0400

    set up eslint/prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,25 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import antfu from "@antfu/eslint-config";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
+export default antfu({
+	stylistic: false,
+	react: true,
+	nextjs: true,
+	rules: {
+		"perfectionist/sort-imports": "warn",
+	},
 });
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
-
-export default eslintConfig;
