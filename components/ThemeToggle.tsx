137317c2c9a4304b7c17d2a385a526cb79d4acba
Oliver Rose <hello@olivermrose.com>
2025-09-18 10:30:10 +0700

    feat: add header (#1)

---
+++ components/ThemeToggle.tsx
@@ -0,0 +1,24 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useCallback, useEffect, useState } from "react";
+import { Button } from "./ui/button";
+
+export default function ThemeToggle() {
+	const [mounted, setMounted] = useState(false);
+	const { resolvedTheme, setTheme } = useTheme();
+
+	useEffect(() => {
+		// eslint-disable-next-line react-hooks-extra/no-direct-set-state-in-use-effect
+		setMounted(true);
+	}, []);
+
+	const toggleTheme = useCallback(() => {
+		setTheme(resolvedTheme === "dark" ? "light" : "dark");
+	}, [resolvedTheme, setTheme]);
+
+	return mounted ? (
+		<Button size="icon" variant="ghost" onClick={toggleTheme}>
+			{resolvedTheme === "dark" ? <Moon /> : <Sun />}
+		</Button>
+	) : null;
+}
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,5 @@ export default antfu({
 	rules: {
 		"perfectionist/sort-imports": "warn",
 	},
+	ignores: ["./components/ui/**/*"],
 });
