d961d4d8f46d55c23baefc3f5a4e51ec23f9746b
yanyancai <hy1033@wayne.edu>
2025-09-20 13:36:41 -0400

    add onboarding

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,66 @@
+"use client";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+export default function Onboarding() {
+  const [step, setStep] = useState<1 | 2>(1);
+  const router = useRouter();
+  const [school,setSchool] = useState("");
+
+useEffect(() => {
+    if (step === 1) {
+      const timer = setTimeout(() => setStep(2), 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [step]);
+
+  return (
+    <div>
+      {step === 1 ? (
+        <div className="w-full min-h-[calc(100dvh-56px)] flex items-center justify-center text-white"
+          style={{ background: "linear-gradient(180deg,#4FD1C5,#319795)" }}
+        >
+          <div className="flex flex-col items-center gap-6">
+            <Image src="/Logoonboarding.svg" alt="StudyRez" width={128} height={128} priority />
+           
+          </div>
+        </div>
+    
+      ) : (
+        <div className="w-full min-h-[calc(100dvh-56px)] flex items-center justify-center bg-[#319795]">
+          <div className="w-full max-w-[420px] px-4">
+            <div className="w-full flex flex-col items-center">
+             <Image src="/Logologin.svg" alt="StudyRez" width={128} height={128} priority />
+
+         <h1 className="mt-4 text-xl font-semibold text-gray-900 text-center">
+            Select your school
+         </h1>
+
+          <div className="mt-6 w-full space-y-4">
+            <input
+              list="school-list"
+              value={school}
+              onChange={(e) => setSchool(e.target.value)}
+              placeholder="Search schools"
+              className="w-full rounded-lg border border-gray-500 px-4 py-3 text-sm focus:border-[#319795] focus:outline-none bg-white"
+            />
+            <datalist id="school-list">
+                <option value="Wayne State University"/>
+            </datalist>
+
+            <button
+              onClick={() => router.push("/login")}
+              className="w-full rounded-full bg-[#1A202C] py-3 text-sm font-medium text-white hover:bg-[#2D3748]"
+            >
+              Continue
+            </button>
+           </div>
+          </div>
+         </div>
+        </div>
+       )}
+    </div>
+  );
+}
+
