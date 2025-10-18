707eb33b8fb3cacd8af7c4ae73e6b72c1205a098
SMiok573 <140364509+SMiok573@users.noreply.github.com>
2025-10-18 19:08:25 -0400

    Add connection for drizzle to postgres

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,4 +1,9 @@
-import process from "node:process";
 import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
-export const db = drizzle(process.env.DATABASE_URL!);
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
+
+const client = postgres(process.env.DATABASE_URL!);
+export const db = drizzle(client);
