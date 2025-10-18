e64a408ea88201a9ec7b934911fcd19f70350913
haniah-wayne <hl1263@wayne.edu>
2025-10-18 12:30:40 -0400

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,7 +1,10 @@
-import type { ClassValue } from "clsx";
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+/**
+ * Utility function to merge Tailwind and conditional class names.
+ * Used throughout all shadcn/ui and Study Buddy components.
+ */
+export function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs));
 }
