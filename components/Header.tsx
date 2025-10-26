2b051e14aef5263c4e441acca4b5198469fd4790
Oliver Rose <hello@olivermrose.com>
2025-10-26 01:14:58 -0400

    feat: shrink header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { BookOpen, ChevronRight } from "lucide-react";
 import Link from "next/link";
+import { useEffect, useRef } from "react";
 import { authClient } from "@/lib/auth/client";
 import ThemeToggle from "./ThemeToggle";
 import { Button } from "./ui/button";
@@ -15,10 +16,27 @@ import {
 } from "./ui/dropdown-menu";
 
 export default function Header() {
+	const headerRef = useRef<HTMLElement>(null);
 	const { data } = authClient.useSession();
 
+	useEffect(() => {
+		function handleScroll() {
+			if (!headerRef.current) return;
+
+			if (window.scrollY > 300) {
+				headerRef.current.classList.add("scrolled");
+			} else {
+				headerRef.current.classList.remove("scrolled");
+			}
+		}
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
 	return (
-		<header className="fixed inset-x-0 top-3 z-50 px-4">
+		<header className="fixed inset-x-0 top-3 z-50 px-4" ref={headerRef}>
 			<div className="bg-card mx-auto max-w-5xl rounded-2xl border p-3 shadow-xs">
 				<div className="flex items-center justify-between">
 					<Link className="flex items-center gap-x-2" href="/">
