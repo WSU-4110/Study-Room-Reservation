fb0e89d1bd47e9b41f24c8733cfc37bb21445460
Oliver Rose <hello@olivermrose.com>
2025-10-26 20:39:03 -0400

    feat: filter rooms

--- app/book/Location.tsx
+++ app/book/Location.tsx
@@ -1,5 +1,5 @@
 import type { Building } from "@/lib/db/schema";
-import { MapPin, Search } from "lucide-react";
+import { MapPin, UsersRound } from "lucide-react";
 import Image from "next/image";
 import { useState } from "react";
 import useSWR from "swr";
@@ -13,7 +13,6 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
 import {
 	Select,
 	SelectContent,
@@ -47,13 +46,56 @@ function Intro() {
 }
 
 export default function Location() {
-	const [query, setQuery] = useState("");
 	const booking = useBooking();
+
+	const [buildingFilter, setBuildingFilter] = useState("all");
+	const [capacityFilter, setCapacityFilter] = useState("all");
 
 	const { data: rooms = [], isLoading } = useSWR<Room[]>(
 		"/api/rooms",
 		fetcher,
 	);
+
+	const buildings = rooms
+		.map((room) => room.building)
+		.filter((building, index, self) => {
+			return index === self.findIndex((b) => b.id === building.id);
+		});
+
+	const filteredRooms = rooms.filter((room) => {
+		if (
+			buildingFilter !== "all" &&
+			room.building.id !== Number(buildingFilter)
+		) {
+			return false;
+		}
+
+		if (capacityFilter !== "all") {
+			const capacity = room.capacity ?? 0;
+
+			switch (capacityFilter) {
+				case "sm": {
+					if (capacity > 4) return false;
+					break;
+				}
+
+				case "md": {
+					if (capacity <= 4 || capacity > 10) return false;
+					break;
+				}
+
+				case "lg": {
+					if (capacity <= 10) return false;
+					break;
+				}
+
+				default:
+					break;
+			}
+		}
+
+		return true;
+	});
 
 	function selectRoom(room: Room) {
 		booking.setLocation(room.building, {
@@ -78,45 +120,47 @@ export default function Location() {
 			<Intro />
 
 			<div className="flex flex-col gap-2 sm:flex-row">
-				<div className="relative w-full">
-					<Search className="text-muted-foreground absolute top-1/2 left-3 size-4 -translate-y-1/2 transform" />
-
-					<Input
-						className="pl-9"
-						value={query}
-						placeholder="Search rooms..."
-						onChange={(event) => setQuery(event.target.value)}
-					/>
-				</div>
-
-				<Select>
+				<Select
+					value={buildingFilter}
+					onValueChange={setBuildingFilter}
+				>
 					<SelectTrigger className="w-full sm:w-auto">
 						<SelectValue placeholder="Filter building" />
 					</SelectTrigger>
 
 					<SelectContent>
 						<SelectItem value="all">All buildings</SelectItem>
-						<SelectItem value="ugl">
-							Undergraduate Library
-						</SelectItem>
-						<SelectItem value="state-hall">State Hall</SelectItem>
-						<SelectItem value="stem">STEM Center</SelectItem>
+
+						{buildings.map((building) => (
+							<SelectItem
+								key={building.id}
+								value={building.id.toString()}
+							>
+								{building.name}
+							</SelectItem>
+						))}
 					</SelectContent>
 				</Select>
 
-				<Select>
+				<Select
+					value={capacityFilter}
+					onValueChange={setCapacityFilter}
+				>
 					<SelectTrigger className="w-full sm:w-auto">
-						<SelectValue placeholder="Filter size" />
+						<SelectValue placeholder="Filter capacity" />
 					</SelectTrigger>
 
 					<SelectContent>
 						<SelectItem value="all">All sizes</SelectItem>
+						<SelectItem value="sm">Small (1-4)</SelectItem>
+						<SelectItem value="md">Medium (4-10)</SelectItem>
+						<SelectItem value="lg">Large (10+)</SelectItem>
 					</SelectContent>
 				</Select>
 			</div>
 
 			<div className="grid-auto grid gap-4">
-				{rooms.map((room) => {
+				{filteredRooms.map((room) => {
 					return (
 						<Card className="overflow-hidden pt-0" key={room.id}>
 							<Image
@@ -137,9 +181,14 @@ export default function Location() {
 									Room {room.number}
 								</CardTitle>
 
-								<CardDescription>
-									Anim deserunt quis laborum Lorem nisi sunt
-									non laborum tempor proident.
+								<CardDescription className="mt-2">
+									<div className="flex items-center">
+										<UsersRound className="mr-1 size-4" />
+
+										{room.capacity
+											? `Accommodates up to ${room.capacity} people`
+											: "Capacity not specified"}
+									</div>
 								</CardDescription>
 							</CardHeader>
 
